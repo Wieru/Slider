28c33da16ab8c7d9fa36136fdc84a7ed3c1e58a7
Szymon Wierucki <szymon.wierucki@gmail.com>
2023-10-30 16:04:30 +0100

    Add arrows logic

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ class Slider {
         this.prevBtn = null;
         this.nextBtn = null;
         this.image = null;
+        this.currentSlide = 0;
+        this.slideArrayLenght = 0;
 
         this.UiSelectors = {
             slide: '[data-slide]',
@@ -20,8 +22,43 @@ class Slider {
 
         this.image = document.createElement('img');
         this.image.classList.add('slide__image');
-        this.image.setAttribute('src', this.images[0]);
+
+        this.setSlideAttributes(0);
+        
+        this.slideArrayLenght = this.images && this.images.length
 
         this.slide.appendChild(this.image);
+        this.disableButtons()
+        this.addListeners()
+    }
+
+    addListeners() {
+        this.prevBtn.addEventListener('click', () => 
+        this.changeSlide(this.currentSlide - 1),
+        );
+        this.nextBtn.addEventListener('click', () => 
+        this.changeSlide(this.currentSlide + 1),
+        );
+    }
+
+    disableButtons() {
+        this.currentSlide === 0 
+            ? this.prevBtn.setAttribute('disabled', true) 
+            : this.prevBtn.removeAttribute('disabled');
+        this.currentSlide === this.slideArrayLenght - 1
+            ? this.nextBtn.setAttribute('disabled', true) 
+            : this.nextBtn.removeAttribute('disabled');
+    }
+
+    changeSlide(index) {
+        this.currentSlide = index;
+
+        this.setSlideAttributes(index);
+        this.disableButtons();
+    }
+
+    setSlideAttributes(index) {
+        this.image.setAttribute('src', Array.isArray(this.images) && this.images.length && this.images[index]);
+        this.image.setAttribute('alt', `Slide ${index + 1}`)
     }
 }
